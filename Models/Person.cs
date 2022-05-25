07f3ecfb3ac4ce71facb78d4140b03b5a0240d7c
davidsteinbruch <davidsteinb@gmail.com>
2022-05-25 16:07:27 -0400

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2.Models
+{
+    public class Person
+    {
+        [StringLength(50)]        
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+    }
+}
